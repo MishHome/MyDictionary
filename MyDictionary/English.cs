310ed97eb3efce49ba90a090c17998c606f466f6
Micha Ostapchuk <ostapchuk.mish@yandex.ru>
2022-10-29 15:55:44 +0300

    Создали класс English и реализовали метод сравнения

---
+++ MyDictionary/English.cs
@@ -0,0 +1,40 @@
+﻿namespace MyDictionary
+{
+    public class English
+    {
+       
+
+        public string Word { get; private set; }
+        public List<string> ListTranslation { get; private set; }
+        public string? Comment { get; private set; }
+
+        public English(string word, List<string> translation, string? comment=null)
+        {
+            Word = word;
+            ListTranslation = translation;
+            Comment = comment;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is English eng) 
+            {
+                if (eng.Word.Equals(this.Word))
+                {
+                    foreach (var trans in eng.ListTranslation)
+                    {
+                        if (this.ListTranslation.Contains(trans))
+                            return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Word, ListTranslation);
+        }
+
+    }
+}
--- MyDictionary/Program.cs
+++ MyDictionary/Program.cs
@@ -1,42 +1,51 @@
-﻿using System.Reflection.Metadata.Ecma335;
+﻿using MyDictionary;
 
 class Program
 {
     static void Main(string[] args)
     {
-        int error = 0;
-        var mydic = MyDictionary.MyDictionary.My_english_words;
-        var dicError= new Dictionary<string, string>();
-        int my_english_words_count = mydic.Count;
-  
-        foreach (var word in mydic)
+         int error = 0;
+         var mydic = MyDictionary.MyDictionary.My_english_words;
+        // var dicError= new Dictionary<string, string>();
+        // int my_english_words_count = mydic.Count;
+
+        List<English> listDictionary= new List<English>();
+        foreach (var dic in mydic)
+        {
+            listDictionary.Add(new English(dic.Key, dic.Value.Split(',').ToList()));
+        }
+
+        foreach (var EngDictionary in listDictionary)
         {
             int count_word_question = 0;
             while (true)
             {
-                Console.Write($" {word.Key} - ");
-                string? UserInputWordtranslation = Console.ReadLine();
-                if (UserInputWordtranslation == null)
+                Console.Write($" {EngDictionary.Word} - ");
+                string? UserInputTranslationChecked = Console.ReadLine();
+                if (UserInputTranslationChecked == null)
                     continue;
-                else if (MyDictionary.MyDictionary.WordVerification(word.Value, UserInputWordtranslation))
+
+                var translateEng = new English(EngDictionary.Word, UserInputTranslationChecked.Split(',').ToList());
+                
+                if (EngDictionary.Equals(translateEng))
                     break;
                 else
                 {
-                   if(dicError.ContainsKey(word.Key) == false)
-                        dicError.Add(word.Key, word.Value);
-                    if (count_word_question >= 2)
-                        break;
+                   //if(dicError.ContainsKey(word.Key) == false)
+                   //     dicError.Add(word.Key, word.Value);
+                   // if (count_word_question >= 2)
+                   //     break;
                     count_word_question++;
                     error++;
                     continue;
                 }
             }
         }
-        Console.WriteLine($"Всего слов {my_english_words_count}, вы ошиблись {error} раз");
-        foreach(var word in dicError)
-        {
-            Console.WriteLine($" {word.Key} - {word.Value} ");
-        }
+        //Console.WriteLine($"Всего слов {my_english_words_count}, вы ошиблись {error} раз");
+        //foreach(var word in dicError)
+        //{
+        //    Console.WriteLine($" {word.Key} - {word.Value} ");
+        //}
         Console.ReadLine();
     }
 
