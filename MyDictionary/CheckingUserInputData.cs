1af84052a6e7a31193160cb44a919e409d76a17a
Micha Ostapchuk <ostapchuk.mish@yandex.ru>
2022-11-11 19:06:25 +0300

    Реализовал ввод английского словосочетание разделённого пробелом.

--- MyDictionary/CheckingUserInputData.cs
+++ MyDictionary/CheckingUserInputData.cs
@@ -10,7 +10,7 @@
             if(String.IsNullOrEmpty(CheckString))
                return false;
 
-            char[] engChars = {'a','b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            char[] engChars = {'a','b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',' ' };
             foreach (var ch in CheckString.ToLower())
             {
                 if (engChars.Contains(ch)==false)
